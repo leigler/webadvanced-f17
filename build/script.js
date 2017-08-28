484d6f367b95a43c97e927ff8adf4b393a998e64
Lukas Eigler-Harding <eiglerhardingl@gmail.com>
2017-08-28 02:11:59 -0400

    style and expand function written

--- build/script.js
+++ build/script.js
@@ -1,3 +1,5 @@
+showdown.setOption('noHeaderId', true);
+
 showdown.setOption('customizedHeaderId', true);
 
 $.when($.ajax({
@@ -10,12 +12,59 @@ $.when($.ajax({
 		text      = string,
 		html      = converter.makeHtml(text);
 
-		$( "body" ).append( $( html ) );
+		$( "body" ).append( $( html ) ); //introduce content
+
+		var h3amount = $("h3").length
+		$("h3").each(function(){
+
+
+
+
+
+			var h3index = $( "h3" ).index( this )
+			
+
+
+			
+
+			if (!$(this).next().is("h3")) {
+				$(this).nextUntil("h3").wrapAll("<span class='h3-wrapper'></span>")
+			 	$(this).wrap("<div class='h3-prepend'></div>")
+				$(this).after("<span class='h3-closed'><div id='horizontal'></div><div id='vertical'></div></span>")
+				$(this).css({"display" : "inline"})
+
+			}
+
+			if($this.parent(".h3-prepend").next().next().next().is("h3")){
+				$(this).parent(".h3-prepend").next(".h3-wrapper").css({"display" : "block"})
+				$(this).sibling(".h3-closed").remove()
+			}
+
+ 			// if(h3index != h3amount -1 && !$(this).next().is("h3")){
+
+			 // 	$(this).nextUntil("h3").wrapAll("<span class='h3-wrapper'></span>")
+			 // 	$(this).wrap("<div class='h3-prepend'></div>")
+				// $(this).after("<span class='h3-closed'><div id='horizontal'></div><div id='vertical'></div></span>")
+				// $(this).css({"display" : "inline"})
+				
+			 // } 
+
+
+		})
     }
  })).then(function(){
 
 	$("body").css({"opacity" : 1, 
 					"transform" : "translateY(0px)"})
+
+
+	$(".h3-closed").click(function(){
+
+		$(this).parent(".h3-prepend").next(".h3-wrapper").css({"display" : "block"})
+		$(this).remove()
+
+	})
+
 
 })
 
--- contents/contents.md
+++ contents/contents.md
@@ -15,14 +15,11 @@ This course will focus on extending students&rsquo; knowledge of code to we
 ## Projects {projects}
 
 ## Schedule {schedule}
-As the semester continues, this section will be filled with notes and links relevant to each class. 
+#### As the semester continues, this section will be filled with notes and links relevant to each class. 
 ### Sept 1
-#### Introductions and some stuff like that
-Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec ullamcorper nulla non metus auctor fringilla.
-#### the second h4
 
 ### Sept 8
-#### and then i added stuff here
+
 hurray hurray
 ### Sept 15
 ### Sept 22
@@ -35,7 +32,7 @@ hurray hurray
 ### Nov 10
 ### Nov 17
 ### Nov 24
-([🦃](https://en.wikipedia.org/wiki/Thanksgiving)— no class)
+#### ([🦃](https://en.wikipedia.org/wiki/Thanksgiving)— no class)
 ### Dec 1
 ### Dec 8
 ### Dec 15
